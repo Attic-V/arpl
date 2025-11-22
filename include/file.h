b808a25925efe43cb46aa5fd97ed07791e9119f2
Attic V <106038832+Attic-V@users.noreply.github.com>
2025-11-22 12:13:55 -0600

    feat: remove everything

--- include/file.h
+++
@@ -1,6 +0,0 @@
-#ifndef file_h
-#define file_h
-
-char *readFile (char *path);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,24 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "data.h"
-#include "file.h"
-#include "memory.h"
-#include "scanner.h"
-#include "token.h"
-
 int main (int argc, char **argv)
 {
-	if (argc != 2) {
-		printf("usage: %s <path>\n", argv[0]);
-		return 1;
-	}
-
-	char *path = argv[1];
-
-	char *source = readFile(path);
-
-	mem_free();
-
 	return 0;
 }
+
