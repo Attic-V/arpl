2d559bcffb882e6cdee4c46d4c632d5f9cac7508
Attic V <106038832+Attic-V@users.noreply.github.com>
2025-11-24 18:19:57 -0600

    style: reformat switch

--- src/modules/token/token.c
+++ src/modules/token/token.c
@@ -3,10 +3,8 @@
 char *token_getLabel (enum token_type type)
 {
 	switch (type) {
-		case token_type_eof:
-			return "eof";
-		default:
-			return "unknown";
+		case token_type_eof: return "eof";
+		default: return "unknown";
 	}
 }
 
